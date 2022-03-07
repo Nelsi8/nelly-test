e938de91c748b3ba9cbe1e5b028efc178b06e809
Nelly <95856434+Nelsi8@users.noreply.github.com>
2022-03-07 16:56:00 +0000

    Add files via upload
    Nelly Wejo Tech test

---
+++ csvparser.py
@@ -0,0 +1,11 @@
+def read_row(file):
+    next_line = file.readline()
+
+    if len(next_line) > 0:
+        return parse_row(next_line)
+
+    return None
+
+def parse_row(next_line):
+    return ''
+
---
+++ test_csvparser.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import Mock
+import csvparser
+
+class TestParser(unittest.TestCase):
+    def test_read_row(self):
+        file = Mock()
+        file.configure_mock(**{'readline.return_value':'\'Vehicle1\',\'Vehicle2\''})
+        
+        #expect
+        expected = ("'Vehicle1' - 'Vehicle2'")
+        
+        #result
+        result = csvparser.read_row(file)
+        print(result)
+        
+        #assert
+        assert result == expected
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
